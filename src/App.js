2e3698e3813a6f5cc8e3265b6b0cdd5aee94fd7f
Pradumna <pradumnabajoria@gmail.com>
2021-03-17 13:58:10 +0530

    sidebar completed

--- src/App.js
+++ src/App.js
@@ -33,6 +33,15 @@ function App() {
           user: user
         })
       })
+
+      spotify.getUserPlaylists()
+        .then((playlists) => {
+          dispatch({
+            type: "SET_PLAYLISTS",
+            playlists: playlists
+          })
+        })
+
     }
   }, [])
 
--- src/reducer.js
+++ src/reducer.js
@@ -3,10 +3,10 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    //token: null
+    token: null
 
     //remove this later
-    token: "BQDmJPRPxo3FihcnUpBB7sdk3m8siscz0ltdtABLFpIrOv5sgDTn6N-GJoBvG3YRdzYJeDtDZOa5UvNduoLs--3Ko9uH6rbXdiXm93ZluUuOapzbRKtsoYXzm1UMJ24nDi8hwuvQvImBJ-EA8SCOWpd9_d9hjpBBPTCFLPndXR-aPzZr"
+    //token: "BQDmJPRPxo3FihcnUpBB7sdk3m8siscz0ltdtABLFpIrOv5sgDTn6N-GJoBvG3YRdzYJeDtDZOa5UvNduoLs--3Ko9uH6rbXdiXm93ZluUuOapzbRKtsoYXzm1UMJ24nDi8hwuvQvImBJ-EA8SCOWpd9_d9hjpBBPTCFLPndXR-aPzZr"
 }
 
 const reducer = (state, action) => {
@@ -24,6 +24,11 @@ const reducer = (state, action) => {
             return{
                 ...state,
                 token: action.token
+            }
+        case "SET_PLAYLISTS":
+            return{
+                ...state,
+                playlists: action.playlists
             }
         default:
             return state;
